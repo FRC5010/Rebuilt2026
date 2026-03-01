26c08951502ee74632132d97dc5423a4d71c6f83
Kyle Heaton <kyle.f.heaton@gmail.com>
2026-02-28 21:39:33 -0500

    Remove old year code and configurations

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 342;
-  public static final String GIT_SHA = "922e18bd9aae389324fdeb5fd6003366c11513f7";
-  public static final String GIT_DATE = "2026-02-28 09:44:36 EST";
+  public static final int GIT_REVISION = 388;
+  public static final String GIT_SHA = "63d1b9a4c9ea665282627233609463adb908f022";
+  public static final String GIT_DATE = "2026-02-28 16:28:05 EST";
   public static final String GIT_BRANCH = "Heaton_Rebuilt";
-  public static final String BUILD_DATE = "2026-02-28 13:48:57 EST";
-  public static final long BUILD_UNIX_TIME = 1772304537141L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-02-28 16:28:58 EST";
+  public static final long BUILD_UNIX_TIME = 1772314138368L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
