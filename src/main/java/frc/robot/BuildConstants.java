566b2385c1c51b5897a9f8e648bb193db5cf70ed
5010 Tiger Dynasty <roboticsteam.fhs@gmail.com>
2026-04-14 16:48:37 -0400

    Turret Rezero Once More

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 560;
-  public static final String GIT_SHA = "377778c806f40b0e761a0c14b47e854a7eceb2c7";
-  public static final String GIT_DATE = "2026-04-14 15:53:23 EDT";
+  public static final int GIT_REVISION = 567;
+  public static final String GIT_SHA = "54738e642b4193978aa3a2573800729ae97b2f4d";
+  public static final String GIT_DATE = "2026-04-14 16:41:07 EDT";
   public static final String GIT_BRANCH = "improvement/shuttle-make-gooderer";
-  public static final String BUILD_DATE = "2026-04-14 16:20:43 EDT";
-  public static final long BUILD_UNIX_TIME = 1776198043363L;
+  public static final String BUILD_DATE = "2026-04-14 16:48:22 EDT";
+  public static final long BUILD_UNIX_TIME = 1776199702548L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
